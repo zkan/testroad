17b29f8d6a8d45ab3ca071d4a07dc73da0bc5441
zkan <zkan.cs@gmail.com>
2020-04-18 11:32:21 +0700

    :sparkles: Register projects to admin :bear:

--- api/testroad/projects/admin.py
+++ api/testroad/projects/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'created',
+        'modified',
+    )
+    search_fields = (
+        'name',
+    )
